92ce5e546f90b11e96e35f413cf0d0183dd6d203
Julio Mihali <mihalijulio@gmail.com>
2020-07-26 21:06:23 +0100

    config changes

--- admin/config.php
+++ admin/config.php
@@ -1,18 +1,18 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/OpenCart/OpenCart/admin/');
-define('HTTP_CATALOG', 'http://localhost/OpenCart/OpenCart/');
+define('HTTP_SERVER', 'http://opencart-447800-1402268.cloudwaysapps.com/admin/');
+define('HTTP_CATALOG', 'http://opencart-447800-1402268.cloudwaysapps.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/OpenCart/OpenCart/admin/');
-define('HTTPS_CATALOG', 'http://localhost/OpenCart/OpenCart/');
+define('HTTPS_SERVER', 'http://opencart-447800-1402268.cloudwaysapps.com/admin/');
+define('HTTPS_CATALOG', 'http://opencart-447800-1402268.cloudwaysapps.com/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/opencart/OpenCart/admin/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/opencart/OpenCart/system/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/opencart/OpenCart/image/');
-define('DIR_STORAGE', 'C:/xampp/storage/');
-define('DIR_CATALOG', 'C:/xampp/htdocs/opencart/OpenCart/catalog/');
+define('DIR_APPLICATION', '/home/447800.cloudwaysapps.com/chxcqckfzd/public_html/admin/');
+define('DIR_SYSTEM', '/home/447800.cloudwaysapps.com/chxcqckfzd/public_html/system/');
+define('DIR_IMAGE', '/home/447800.cloudwaysapps.com/chxcqckfzd/public_html/image/');
+define('DIR_STORAGE', '/home/447800.cloudwaysapps.com/chxcqckfzd/public_html/storage/');
+define('DIR_CATALOG', '/home/447800.cloudwaysapps.com/chxcqckfzd/public_html/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -26,8 +26,8 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'chxcqckfzd');
+define('DB_PASSWORD', 'zwqWMv7Jvk');
 define('DB_DATABASE', 'opencart');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
