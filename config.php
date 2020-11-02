1f1bb8df3396a49131f08122fb01e0910db9095f
MAC <mac@MACs-MacBook-Pro.local>
2020-11-02 21:37:13 +0000

    config changes

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://opencart-447800-1402268.cloudwaysapps.com/');
+define('HTTP_SERVER', 'http://opencart-496918-1571105.cloudwaysapps.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://opencart-447800-1402268.cloudwaysapps.com/');
+define('HTTPS_SERVER', 'http://opencart-496918-1571105.cloudwaysapps.com/');
 
 
 // DIR
-define('DIR_APPLICATION', '/home/447800.cloudwaysapps.com/pmbucdkewq/public_html/catalog/');
-define('DIR_SYSTEM', '/home/447800.cloudwaysapps.com/pmbucdkewq/public_html/system/');
-define('DIR_IMAGE', '/home/447800.cloudwaysapps.com/pmbucdkewq/public_html/image/');
-define('DIR_STORAGE', '/home/447800.cloudwaysapps.com/cpmbucdkewq/public_html/storage/');
+define('DIR_APPLICATION', '/home/496918.cloudwaysapps.com/pmbucdkewq/public_html/catalog/');
+define('DIR_SYSTEM', '/home/496918.cloudwaysapps.com/pmbucdkewq/public_html/system/');
+define('DIR_IMAGE', '/home/496918.cloudwaysapps.com/pmbucdkewq/public_html/image/');
+define('DIR_STORAGE', '/home/496918.cloudwaysapps.com/pmbucdkewq/public_html/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -25,7 +25,7 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'pmbucdkewq');
-define('DB_PASSWORD', 'Ej2jFZts6t');
+define('DB_PASSWORD', 'f4ekgn6msV');
 define('DB_DATABASE', 'pmbucdkewq');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
