ea0f480d58c15a46d78341e7aa33e28208c90e12
MAC <mac@MACs-MacBook-Pro.local>
2020-11-02 21:55:54 +0000

    admin-config changes

--- admin/config.php
+++ admin/config.php
@@ -1,18 +1,18 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://opencart-447800-1402268.cloudwaysapps.com/admin/');
-define('HTTP_CATALOG', 'http://opencart-447800-1402268.cloudwaysapps.com/');
+define('HTTP_SERVER', 'http://opencart-496918-1571105.cloudwaysapps.com/admin/');
+define('HTTP_CATALOG', 'http://opencart-496918-1571105.cloudwaysapps.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://opencart-447800-1402268.cloudwaysapps.com/admin/');
-define('HTTPS_CATALOG', 'http://opencart-447800-1402268.cloudwaysapps.com/');
+define('HTTPS_SERVER', 'http://opencart-496918-1571105.cloudwaysapps.com/admin/');
+define('HTTPS_CATALOG', 'http://opencart-496918-1571105.cloudwaysapps.com/');
 
 // DIR
-define('DIR_APPLICATION', '/home/447800.cloudwaysapps.com/chxcqckfzd/public_html/admin/');
-define('DIR_SYSTEM', '/home/447800.cloudwaysapps.com/chxcqckfzd/public_html/system/');
-define('DIR_IMAGE', '/home/447800.cloudwaysapps.com/chxcqckfzd/public_html/image/');
-define('DIR_STORAGE', '/home/447800.cloudwaysapps.com/chxcqckfzd/public_html/storage/');
-define('DIR_CATALOG', '/home/447800.cloudwaysapps.com/chxcqckfzd/public_html/catalog/');
+define('DIR_APPLICATION', '/home/496918.cloudwaysapps.com/pmbucdkewq/public_html/admin/');
+define('DIR_SYSTEM', '/home/496918.cloudwaysapps.com/pmbucdkewqpublic_html/system/');
+define('DIR_IMAGE', '/home/496918.cloudwaysapps.com/pmbucdkewq/public_html/image/');
+define('DIR_STORAGE', '/home/496918.cloudwaysapps.com/pmbucdkewq/public_html/storage/');
+define('DIR_CATALOG', '/home/496918.cloudwaysapps.com/pmbucdkewq/public_html/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -26,9 +26,9 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'chxcqckfzd');
+define('DB_USERNAME', 'pmbucdkewq');
 define('DB_PASSWORD', 'f4ekgn6msV');
-define('DB_DATABASE', 'chxcqckfzd');
+define('DB_DATABASE', 'pmbucdkewq');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
